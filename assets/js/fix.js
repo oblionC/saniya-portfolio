c0dfbc01f4f5854f01f725e607233cfa1699e942
oblionC <tanmaythegreat17@gmail.com>
2025-03-06 22:15:00 +0530

    bruh

--- assets/js/fix.js
+++
@@ -1,6 +0,0 @@
-var imgs = document.getElementsByTagName("img")
-for(var img of imgs) {
-    if(img.getAttribute("src")[0] !== "/") {
-        img.setAttribute("src", "/" + img.getAttribute("src"))
-    }
-}
