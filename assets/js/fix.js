565bef3c3d0937f5422f77002de5dd124ab30d55
oblionC <tanmaythegreat17@gmail.com>
2025-03-06 21:59:08 +0530

    pleaseeee

--- assets/js/fix.js
+++ assets/js/fix.js
@@ -1,6 +1,7 @@
 var imgs = document.getElementsByTagName("img")
 for(var img of imgs) {
     if(img.getAttribute("src")[0] !== "/") {
+        console.log(img)
         img.setAttribute("src", "/" + img.getAttribute("src"))
     }
 }
